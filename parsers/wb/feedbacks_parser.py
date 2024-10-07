b64aef7f21522e7e8faf4848fd1467cbe19dc24b
kirill737 <kirill737apple@yandex.ru>
2024-10-08 00:55:25 +0300

    small fix

--- parsers/wb/feedbacks_parser.py
+++ parsers/wb/feedbacks_parser.py
@@ -1,11 +1,7 @@
 """
 DOCSTRING TEMPLATE
 """
-import json
-import re
-from pathlib import Path
 import requests
-from models import InputJSON, OutputProduct
 
 def get_root_by_id(product_id: int) -> int:
     response = requests.get(
@@ -25,26 +21,27 @@ def get_root_by_id(product_id: int) -> int:
     except (KeyError, IndexError) as exc:
         raise RuntimeError("Json access error") from exc
 
-def get_raw_feedbacks_by_root(root: int) -> list[str]:
+def get_feedbacks_by_root(root: int) -> list[str]:
     response = requests.get(
         f"https://feedbacks1.wb.ru/feedbacks/v2/{root}",
         timeout=10
     )
-    feedbacks_list = []
     if response.status_code != 200:
         raise RuntimeError(
             f"Wrong response code: {response.status_code}"
         )
     feedbacks_json = response.json()
     try:
-        # feedbacks_count = feedbacks_json['feedbackCount']
+        feedbacks_list = []
         for feedback in feedbacks_json['feedbacks']:
             feedbacks_list.append(feedback['text'])
-            
         return feedbacks_list
     except (KeyError, IndexError) as exc:
         raise RuntimeError("Json access error") from exc
-root = get_root_by_id(179755083)
-print(root)
-feedbacks = get_raw_feedbacks_by_root(root)
-print(feedbacks)
+
+def get_all_feedbacks(product_id: int) -> list[str]:
+    root = get_root_by_id(product_id)
+    feedbacks = get_feedbacks_by_root(root)
+    return feedbacks
+
+print(get_all_feedbacks)
