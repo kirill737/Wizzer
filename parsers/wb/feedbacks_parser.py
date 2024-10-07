1f7a8a36dd214c48c9a50e132db1ae0a5f40415c
kirill737 <kirill737apple@yandex.ru>
2024-10-08 00:44:29 +0300

    add feedbacks parser

--- parsers/wb/feedbacks_parser.py
+++ parsers/wb/feedbacks_parser.py
@@ -1,58 +1,50 @@
-from bs4 import BeautifulSoup
-import requests
+"""
+DOCSTRING TEMPLATE
+"""
 import json
+import re
+from pathlib import Path
+import requests
+from models import InputJSON, OutputProduct
 
-# Чтобы сработало надо создать файл со своими куками в json
-with open('cookies.json', 'r', encoding='utf-8') as file:
-    cookies = json.load(file)
+def get_root_by_id(product_id: int) -> int:
+    response = requests.get(
+        f"https://card.wb.ru/cards/v2/detail?"
+        f"appType=1&curr=rub&dest=-1257786&nm={product_id}",
+        timeout=10
+    )
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Wrong response code: {response.status_code}"
+        )
+    root_json = response.json()
+    print(type(root_json))
+    try:
+        root_value = root_json['data']['products'][0]['root']
+        return root_value
+    except (KeyError, IndexError) as exc:
+        raise RuntimeError("Json access error") from exc
 
-# Заголовки для имитации браузера
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 YaBrowser/24.7.0.0 Safari/537.36',
-}
-
-# URL страницы
-url = 'https://www.wildberries.ru/catalog/179755083/feedbacks?imtId=175097505'
-
-# Отправляем GET-запрос с куками и заголовками
-response = requests.get(url, headers=headers, cookies=cookies)
-
-# Проверяем результат
-if response.status_code == 200:
-    # Сохраняем результат в файл или анализируем с помощью BeautifulSoup
-    with open('wildberries_feedbacks.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
-else:
-    print(f"Ошибка: {response.status_code}")
-
-
-
-
-def parse_feedback_text(url):
-    # response = requests.get(url)
-    with open(url, 'r', encoding='utf-8') as file:
-        html_content = file.read()
-    # Если запрос успешен
-    if response.status_code == 200:
-        # Создаем объект BeautifulSoup для парсинга HTML
-        soup = BeautifulSoup(response.content, 'html.parser')
-        with open('ttt.html', 'w', encoding='utf-8') as file:
-            file.write(response.text)
-        
-        # Ищем все элементы с классом 'feedback__text--item'
-        feedback_items = soup.find_all('span', class_='feedback__text--item')
-        print(feedback_items)
-        # Извлекаем текст из каждого найденного элемента и собираем в список
-        feedback_texts = [item.get_text(strip=True) for item in feedback_items]
-
-        return feedback_texts
-    return f"Ошибка {response.status_code} при запросе страницы"
-
-url = 'https://www.wildberries.ru/catalog/179740419/feedbacks?imtId=173747980&size=297218388'
-
-url = 'wildberries_feedbacks.html'
-feedback_texts = parse_feedback_text(url)
-
-for feedback in feedback_texts:
-    print(feedback)
-
+def get_raw_feedbacks_by_root(root: int) -> list[str]:
+    response = requests.get(
+        f"https://feedbacks1.wb.ru/feedbacks/v2/{root}",
+        timeout=10
+    )
+    feedbacks_list = []
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Wrong response code: {response.status_code}"
+        )
+    feedbacks_json = response.json()
+    try:
+        # feedbacks_count = feedbacks_json['feedbackCount']
+        for feedback in feedbacks_json['feedbacks']:
+            feedbacks_list.append(feedback['text'])
+            
+        return feedbacks_list
+    except (KeyError, IndexError) as exc:
+        raise RuntimeError("Json access error") from exc
+root = get_root_by_id(179755083)
+print(root)
+feedbacks = get_raw_feedbacks_by_root(root)
+print(feedbacks)
