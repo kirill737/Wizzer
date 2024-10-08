b0755617cc096822e671648156ce84393d8151f5
kirill737 <kirill737apple@yandex.ru>
2024-10-08 16:28:47 +0300

    добавил плюсы и минусы

--- parsers/wb/feedbacks_parser.py
+++ parsers/wb/feedbacks_parser.py
@@ -32,21 +32,25 @@ def get_raw_feedbacks_by_root_index(root: int, index: int) -> dict:
     feedbacks_json = response.json()
     return feedbacks_json
 
-def get_feedbacks_by_root(root: int) -> list[str]:
+def get_feedbacks_by_root(root: int) -> list[dict]:
     for i in range(1, 5):
         if get_raw_feedbacks_by_root_index(root, i)['feedbacks']:
             feedbacks_json = get_raw_feedbacks_by_root_index(root, i)
             break
         continue
     try:
-        feedbacks_list = []
+        feedbacks_info = []
         for feedback in feedbacks_json['feedbacks']:
-            feedbacks_list.append(feedback['text'])
-        return feedbacks_list
+            feedbacks_info.append({
+                "text": feedback['text'], 
+                "pros": feedback['pros'],
+                "cons": feedback['cons']
+            })
+        return feedbacks_info
     except (KeyError, IndexError) as exc:
         raise RuntimeError("Json access error") from exc
 
-def get_all_feedbacks(product_id: int) -> list[str]:
+def get_all_feedbacks(product_id: int) -> dict:
     root = get_root_by_id(product_id)
     feedbacks = get_feedbacks_by_root(root)
     return feedbacks
