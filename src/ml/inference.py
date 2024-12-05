d77495d79091ecac247c13ec193e540f1a8e1985
user <kygrachev@hse.ru>
2024-12-05 21:10:02 +0300

    Написал функции для обработки отзывов на оценки

--- src/ml/inference.py
+++ src/ml/inference.py
@@ -1,20 +1,53 @@
+import os
+
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
-def postprocess_logits(logits  ):
+helpers = {
+    "plus": "Преимущества", 
+    "minus": "Недостатки",
+}
+
+
+def postprocess_logits(logits):
     # Преобразуем числа в классы
-    classes = np.where(logits < -0.33, -1, np.where(logits > 0.33, 1, 0))
+    classes = np.where(logits <= -0.33, -1, np.where(logits >= 0.33, 1, 0))
     return classes
 
 
-# Загрузка модели и токенизатора
-model = AutoModelForSequenceClassification.from_pretrained("rubert_multilabel_model-best")
-tokenizer = AutoTokenizer.from_pretrained("rubert_multilabel_model-best")
+def process_texts(model: AutoModelForSequenceClassification, tokenizer: AutoTokenizer, texts: list):
+    # Токенизация всех текстов
+    inputs = tokenizer(texts, return_tensors="pt", truncation=True, padding=True, max_length=128)
+
+    # Прогон текстов через модель
+    outputs = model(**inputs)
+    logits = outputs.logits.detach().cpu().numpy()
+
+    # Постобработка logits
+    return postprocess_logits(logits)
 
 
-text = "преимущества: снимки ОГОНЬ!!!"
-inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=128)
-outputs = model(**inputs)
-predictions = outputs.logits.detach().cpu().numpy()
-predictions
+def process_reviews(reviews):
+    indexes, all_texts = [0], []
+    for review in reviews: 
+        texts = [
+            helpers.get(review_part, "") + " : " + comment 
+            for review_part, comment in review.items() if len(comment) > 4
+        ]
+        indexes.append(indexes[-1] + len(texts))
+        all_texts.extend(texts)
+
+    # Загрузка модели и токенизатора
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    model = AutoModelForSequenceClassification.from_pretrained(os.path.join(script_dir, "rubert_multilabel_model-best"))
+    tokenizer = AutoTokenizer.from_pretrained(os.path.join(script_dir, "rubert_multilabel_model-best"))
+
+    all_marks = process_texts(model, tokenizer, all_texts)
+    
+    review_marks = []
+    for i, j in zip(indexes[:-1], indexes[1:]):
+        review_summary = all_marks[i : j].sum(axis=0)
+        review_marks.append(np.where(review_summary >= 1, 1, np.where(review_summary <= -1, -1, 0)))
+
+    return np.array(review_marks)
